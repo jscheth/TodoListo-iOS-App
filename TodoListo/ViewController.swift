07d0331d3ce6a7c727204cff6e58452466614433
jscheth <sovichea.chet@gmail.com>
2025-04-27 19:44:14 -0700

    Initial Commit

---
+++ TodoListo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TodoListo
+//
+//  Created by Jonathan Cheth on 4/27/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TodoListoTests/TodoListoTests.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListoTests.swift
+//  TodoListoTests
+//
+//  Created by Jonathan Cheth on 4/27/25.
+//
+
+import Testing
+@testable import TodoListo
+
+struct TodoListoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
