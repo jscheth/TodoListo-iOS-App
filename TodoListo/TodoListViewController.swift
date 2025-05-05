8ff04104e6e508e80775866275ae782eac6a5338
jscheth <sovichea.chet@gmail.com>
2025-05-04 21:49:17 -0700

    Add hard code displayed items

--- TodoListo/TodoListViewController.swift
+++ TodoListo/TodoListViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Buy Eggs", "Pet Cat", "Buy Game"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
 }
 
